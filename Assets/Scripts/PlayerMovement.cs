01ee2511b74074e4fb2971b45003e416ab55fe7d
Kairuni <keegan@kairuni.com>
2016-04-18 16:52:55 -0700

    Project Reorganization and fixing .gitignore
    Fixed .gitignore, reorganized assets, added controller simplemove.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,32 +4,16 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
-    private Vector3 moveDirection = Vector3.zero;
-    private float gravity = 20.0f;
 
-	// Use this for initialization
-	void Start () {
+    private CharacterController controller;
 
+    void Awake() {
+        controller = GetComponent<CharacterController>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        CharacterController controller = GetComponent<CharacterController>();
-        /*if(Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector3(0, 0, speed);
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            transform.position = new Vector3(0, 0, -speed);
-        }*/
-      
-            moveDirection = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-        
-
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+       
+        controller.SimpleMove(new Vector3(0, 0, Input.GetAxis("Vertical") * speed));
     }
 }
