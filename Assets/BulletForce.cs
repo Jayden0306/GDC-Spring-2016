fe68cb2483c04c705decb7b342285b726bd696b6
Connor Lundberg <connorlndbrg349@gmail.com>
2016-04-22 13:21:19 -0700

    Shooting bullets
    Added bullets to shoot. Bullets are not to size I made them big so we
    can easily see what they are doing.

---
+++ Assets/BulletForce.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletForce : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.up * speed;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    //Lane size
     public float speed;
     public float spacesToTheLeft;
     public float spacesToTheRight;
     public float laneWidth;
 
+    //Bullet
+    public GameObject bullet;
+    public Transform shotSpawn;
+    
+    //Player movement
     private CharacterController controller;
     private int position;
     private float startZ;
@@ -20,6 +26,11 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //for the bullet instantiation
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, shotSpawn.position, shotSpawn.rotation);
+        }
         //controller.SimpleMove(new Vector3(0, 0, Input.GetAxis("Vertical")) * speed);
 
         if (Input.GetKeyDown(KeyCode.W) && position < spacesToTheLeft)
@@ -38,4 +49,5 @@ public class PlayerMovement : MonoBehaviour {
 
         controller.Move(new Vector3(0, 0, direction) * speed * Time.deltaTime);
     }
+
 }
