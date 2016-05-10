6897cc8033d90ca0e605afe077293dcfaceeddaa
jayden0306 <feng6680946@hotmail.com>
2016-05-09 23:50:27 -0700

    add start menu
    add start and quit button with animation

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class UIManagerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
