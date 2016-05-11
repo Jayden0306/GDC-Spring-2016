a286a14b73a41f3d6bceccd136335cd42a134a90
Kairuni <keegan@kairuni.com>
2016-05-10 22:59:26 -0700

    Relocated Files, Fixed Bugs
    Had a few compile errors from the addition of the aiming cursor when the
    sprite ended up undefined, fixed that.
    
    I'll be commiting another entry in a few hours, almost done with the
    waves bit.

--- Assets/Scripts/PlayerScripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerScripts/PlayerShooting.cs
@@ -6,33 +6,31 @@ public class PlayerShooting : MonoBehaviour {
     public GameObject bullet;
     public Transform shotSpawn;
     public AudioClip bulletShot;
-    public GameObject sprite;
+    public GameObject cursorSprite;
 
     public float shotDelay = 2f;
     float shotTimer = 0f;
 
     float armRotation = 0;
-<<<<<<< HEAD
+
     private bool canShoot;
 
-    public bool GetCanShoot()
-    {
+    public bool GetCanShoot() {
         return canShoot;
     }
 
-    public void SetCanShoot(bool value)
-    {
+    public void SetCanShoot(bool value) {
         canShoot = value;
-=======
+    }
     
     void Start()
     {
-        if (sprite != null)
+        canShoot = true;
+        if (cursorSprite != null)
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Confined;
         }
->>>>>>> origin/master
     }
 
 	// Update is called once per frame
@@ -59,7 +57,8 @@ public class PlayerShooting : MonoBehaviour {
 
         if (Physics.Raycast(shotRay, out hit, Mathf.Infinity, layerMask)) {
             armRotation = Mathf.Atan2(hit.point.y-shotSpawn.transform.position.y, hit.point.x - shotSpawn.transform.position.x) * Mathf.Rad2Deg;
-            sprite.transform.position = hit.point;
+            if (cursorSprite)
+                cursorSprite.transform.position = hit.point;
             //Debug.Log(hit.point.ToString() + " : " + armRotation);
         }
     }
